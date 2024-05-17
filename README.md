6367dde9c2bd5d52a82e76183669b8f0aee950f5
PedroLucas-O <pedrolucas.pc.cp@gmail.com>
2024-05-17 16:52:27 -0300

    aula 13

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1>
+ <b> Album de Fotos </b>
+</h1>
+
+<li>action-sheet</li>
+
+<br>
+
+<img src="./src/assets/tela-1.png"/>
+<br>
+<img src="./src/assets/tela-2.png"/>
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActionSheetButton, ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public actionSheetCtrl:ActionSheetController) {}
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Actions',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          data: {
+            action: 'delete',
+          },
+        },
+        {
+          text: 'Compartilhar',
+          data: {
+            action: 'share',
+          },
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
+
+    await actionSheet.present();
+  }
 
 }
